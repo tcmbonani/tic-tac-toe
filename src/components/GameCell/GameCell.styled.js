95558826a90dc3656bdb6263a691cbfeef6591f3
Clement Mbonani <Clement.Mbonani@vcontractor.co.za>
2024-06-29 18:16:41 +0200

    color change

--- src/components/GameCell/GameCell.styled.js
+++ src/components/GameCell/GameCell.styled.js
@@ -6,11 +6,11 @@ color: ${(props) => props.theme.colors.primary};
 font-size: 3rem;
 border: none;
 width: 10rem;
-height: 8rem;
+height: 9rem;
 border-radius: 10px;
 box-shadow: 5px 10px #455141;
 cursor: pointer; 
-padding:0rem -2.5rem;
+padding:-4rem 3rem;
 
 .outlineIcon{
     path {
