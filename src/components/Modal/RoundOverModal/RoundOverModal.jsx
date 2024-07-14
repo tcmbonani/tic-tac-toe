031d621080db94ffba51003b29bd99e5494c1126
Clement Mbonani <Clement.Mbonani@vcontractor.co.za>
2024-07-14 08:06:46 +0200

    title

--- src/components/Modal/RoundOverModal/RoundOverModal.jsx
+++ src/components/Modal/RoundOverModal/RoundOverModal.jsx
@@ -11,7 +11,7 @@ function RoundOverModal() {
   return (
     <>
     <ModalHeader>
-    <Title>{game.roundWinner.name}Round Complete</Title>
+    <Title>{game.roundWinner ? `${game.roundWinner.name} Wins Round` : "Round Draw" }</Title>
     </ModalHeader>
     <ModalBody>
     <Subtitle> You win !</Subtitle>
